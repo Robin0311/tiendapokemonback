ec5e60a7a479b6d3188f3b983c4df7fab61b8ba0
Robin9203 <robinxd11@gmail.com>
2023-02-09 00:02:16 +0100

    feat: correccion dato login

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ app.get('/', (req, res) =>{
 //Collecciones
 //Customers
 // localhost:5000/customers
-app.use('/customers', require('../src/routers/customers.routers'))
+app.use('/customer', require('../src/routers/customers.routers'))
 
 //shopping
 // localhost:5000/shopping
